810d3ca1f1ff67915ad53ed0a1ffd0694c7015a7
Drawde <60484184+edwardtimoty@users.noreply.github.com>
2021-04-08 23:29:52 +0700

    membuat variabel dan menampilkan ke console log di browser
    7/4/2021

---
+++ 2. Awal belajar js dasar part 2/script.js
@@ -0,0 +1,5 @@
+// deklarasi & inisialisasi dan assigment
+var x = 10;
+// untuk menuliskan sesuatu ke console di browser
+console.log('Hello World');
+console.log('isi dari variabel x adalah ' + x);
