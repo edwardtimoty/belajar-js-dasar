5b1d4aeef93ad491571b079ab78d0b337394c22c
Drawde <60484184+edwardtimoty@users.noreply.github.com>
2021-04-08 23:28:07 +0700

    Delete 2. Awal belajar js dasar part 2 directory

--- 2. Awal belajar js dasar part 2/script.js
+++
@@ -1,5 +0,0 @@
-// deklarasi & inisialisasi dan assigment
-var x = 10;
-// untuk menuliskan sesuatu ke console di browser
-console.log('Hello World');
-console.log('isi dari variabel x adalah ' + x);
