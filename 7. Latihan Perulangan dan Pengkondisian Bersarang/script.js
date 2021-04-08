dfe839fc196b4d622c4a1bc7cccb3b573f1d4f68
Drawde <60484184+edwardtimoty@users.noreply.github.com>
2021-04-08 23:44:01 +0700

    memahami perulangan dan pengkondisian bersarang dengan membuat segitiga bintang
    8/4/2021

---
+++ 7. Latihan Perulangan dan Pengkondisian Bersarang/script.js
@@ -0,0 +1,97 @@
+// Segitiga Bintang Terbalik
+// var s = '';
+// for(var i = 10; i > 0; i--) {
+// for(var j =0; j < i; j++) {
+// s += '*';
+// }
+//     s += '\n';
+// }
+// console.log(s);
+// ===================================
+
+// Segitiga Bintang
+// var s = '';
+// for(var i = 0; i < 10; i++) {
+//     for(var j = 0; j <= i; j++) {
+//         s += '*';
+//     }  
+//     s += '\n'; 
+// }
+// console.log(s);
+// ===================================
+
+// Segitiga terbalik
+// var k = '';
+// for(var a = 0; a <= 10; a++){
+//     for(var c = 0; c < a; c++){
+//         k += ' ';
+//     }
+//     for(var b = a; b <= 10; b++){
+//         k += '*';
+//     }
+//     k += '\n';
+// }
+// console.log(k);
+// ===================================
+
+// Gabungan dua segitiga
+// var a = '';
+// for(var b = 0; b < 10; b++){
+//     for(var c = 0; c <= b; c++){
+//         a += '*';
+//     }
+//     a += '\n';
+// }
+// for(var d = 0; d <= 10; d++){
+//     for(var e = d; e <= 10; e++){
+//         a += '*';
+//     }
+//     a += '\n';
+// }
+// console.log(a);
+// ====================================
+
+// Segitiga sama kaki
+// var a = '';
+// for (var b = 0; b < 10; b++){
+//     for(var d = b; d <= 10; d++){
+//         a += ' ';
+//     }
+//     for(var c = 1; c <= b; c++){
+//         a += '*';
+//     }
+//     for(var e = 1; e <= c; e++){
+//         a += '*';
+//     }
+//     a += '\n';
+// }
+// console.log(a);
+// ====================================
+
+// Gabungan semua segitiga
+// var a = '';
+// for (var b = 0; b < 10; b++){
+//     for(var d = b; d <= 10; d++){
+//         a += ' ';
+//     }
+//     for(var c = 1; c <= b; c++){
+//         a += '*';
+//     }
+//     for(var e = 1; e <= c; e++){
+//         a += '*';
+//     }
+//     a += '\n';
+// }
+// for (var f = 0; f <= 10; f++){
+//     for(var h = 0; h <= f; h++){
+//         a += ' ';
+//     }
+//     for(var g = f; g <= 10; g++){
+//         a += '*';
+//     }
+//     for(var i = h; i <= 10; i++){
+//         a += '*';
+//     }
+//     a += '\n';
+// }
+// console.log(a);
