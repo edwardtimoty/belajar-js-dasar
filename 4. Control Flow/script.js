fbdecbb454a280a7f1afb4404d211ceb027d0380
Drawde <60484184+edwardtimoty@users.noreply.github.com>
2021-04-08 23:35:45 +0700

    memahami control flow pengulangan dan pengkondisian
    7/4/2021

---
+++ 4. Control Flow/script.js
@@ -0,0 +1,14 @@
+// Pengulangan
+// alert('Mulai');
+// for(var i = 0; i < 5; i++) {
+//     alert('Hello World');
+// }
+// alert('Selesai');
+
+// Pengkondisian
+var angka = prompt('Masukkan angka : ');
+if(angka % 2 === 0) {
+    alert(angka + ' adalah bilangan genap');
+} else {
+    alert(angka + ' adalah bilangan ganjil');
+}
