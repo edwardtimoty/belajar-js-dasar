6755aeb15e1bff7c4797f1c73b0927b5e6fdbbd3
Drawde <60484184+edwardtimoty@users.noreply.github.com>
2021-04-08 23:47:13 +0700

    Delete 9. Function directory

--- 9. Function/Declaration/script.js
+++
@@ -1,6 +0,0 @@
-// declaration : function indetifier (ParameterList opt) { FunctionBody }
-// lebih fleksibel(dapat ditulis dimanapun) karena konsep Hoisting, mudah dipahami pemula
-// contoh :
-function tampilPesan (nama) {
-    alert('halo ' + nama );
-}
